a4ae60cf7bb6b92ab48f03a436a3112e54539d99
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2020-03-01 18:46:33 +0600

    Course and Teacher Asign Updated

--- course/admin.py
+++ course/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course,AssignTeacher
 from session.models import Semester
 from programs.models import Department
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-	list_display = ["course_name","course_code","course_teachers","course_credit","semester","department"]
-	search_fields = ('course_name','course_code','teacher','semester','department',)
-	list_filter = ['course_credit','course_code','department','semester']
-	autocomplete_fields = ('department','teacher',)
+	list_display = ["course_name","course_code","course_credit","semester","department"]
+	search_fields = ('course_name','course_code','semester__semester_name','department__department_name',)
+	list_filter = ['course_credit','course_code','department__department_name','semester__semester_name']
+	autocomplete_fields = ('department',)
 	#raw_id_fields = ("teacher",)
 	list_per_page = 20
 
@@ -22,3 +22,25 @@ class CourseAdmin(admin.ModelAdmin):
 			return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 admin.site.register(Course,CourseAdmin)
+
+
+
+class AssignTeacherAdmin(admin.ModelAdmin):
+	list_display = ["course","course_teachers","department","faculty","semester"]
+	search_fields = ('course__course_name','course__course_code','teacher__teacher__username','course__semester__semester_name','course__department__department_name',)
+	list_filter = ['course__course_credit','course__course_code','course__department__department_name','course__semester__semester_full_name']
+	autocomplete_fields = ('course','teacher',)
+	#raw_id_fields = ("teacher",)
+	list_per_page = 20
+
+
+	def department(self,obj):
+		return obj.course.department.department_name
+
+	def faculty(self,obj):
+		return obj.course.department.faculty.faculty_name
+
+	def semester(self,obj):
+		return obj.course.semester.semester_full_name
+
+admin.site.register(AssignTeacher,AssignTeacherAdmin)
--- course/models.py
+++ course/models.py
@@ -7,17 +7,25 @@ from session.models import Semester
 class Course(models.Model):
 	semester   =  models.ForeignKey(Semester,on_delete=models.CASCADE,related_name='semester_courses')
 	department   =  models.ForeignKey(Department,on_delete=models.CASCADE,related_name='department_courses')
-	teacher =  models.ManyToManyField(Teacher,related_name='teacher_courses')
 	course_name  =  models.CharField(max_length=150)
 	course_code  =  models.CharField(max_length=150)
 	course_image =  models.ImageField(upload_to="course/%Y/%m/%d/")
 	course_credit = models.IntegerField()
 
-	def course_teachers(self):
-		return ",".join([str(p) for p in self.teacher.all()])
-
 	def __str__(self):
 		return self.course_name
 
 
+
+class AssignTeacher(models.Model):
+	course = models.ForeignKey(Course,on_delete=models.CASCADE,related_name='course_teacher')
+	teacher =  models.ManyToManyField(Teacher,related_name='teacher_courses')
+
+
+	def course_teachers(self):
+		return ",".join([str(p) for p in self.teacher.all()])
+
+
+	def __str__(self):
+		return self.course.course_name
 
